b82b484220967e297ed2ff350a89ea8fb787c885
Preet Parmar <preet@192.168.1.13>
2025-12-19 15:08:21 +0530

    Configure SMTP email for production

--- carproject/settings.py
+++ carproject/settings.py
@@ -170,14 +170,15 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-if not DEBUG:
-    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-EMAIL_HOST_USER = 'pythoncoding444@gmail.com'
-EMAIL_HOST_PASSWORD = 'eybu jssn mysa brkl'
+#EMAIL_HOST_USER = os.getenv('pythoncoding444@gmail.com')
+#EMAIL_HOST_PASSWORD = 'eybu jssn mysa brkl'
 
-DEFAULT_FROM_EMAIL = 'DriveSurely <pythoncoding444@gmail.com>'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
